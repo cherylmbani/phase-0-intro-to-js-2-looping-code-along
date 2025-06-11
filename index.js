e9031aeada966f2aa410a4afc3977e41bb6d944a
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-06-11 11:39:55 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    const messages=[];
+    for(let i=0; i<names.length; i++){
+        const message =(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        messages.push(message);
+    }
+    return messages;
+
+}
+
+function countDown(num){
+    while(num>=0){
+    console.log(num--);
+}
+}
